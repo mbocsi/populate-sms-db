81d4542ceccc1c6e484cf611e566b234c88cf8bb
Marton Bocsi <mvbocsi@gmail.com>
2024-01-18 23:48:33 -0600

    First iteration of script. Need to figure out updatedAt field.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
-import mysql.connector
+import mysql.connector as mysql
 from dotenv import load_dotenv
 from dataclasses import dataclass
 import requests
 from time import sleep
 import logging
+from bs4 import BeautifulSoup
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO, filename="main.log", encoding="utf-8", format="%(asctime)s %(message)s" )
 
 DB_HOST = os.getenv("DB_HOST")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
 
-ITEMS_URL = "https://steamcommunity.com/market/search/render/"
+ITEMS_URL = "https://steamcommunity.com/market/search/render"
+ITEM_PAGE_URL = "https://steamcommunity.com/market/listings"
 
 @dataclass(slots=1)
 class Item:
@@ -24,18 +27,41 @@ class Item:
     gameId: int
 
 class PopulateItems:
-    def __init__(self, appId : int):
+    def __init__(self, appId : int, start_index : int = 0, page_size : int = 100):
         try:
-            self.connection = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE)
+            self.connection = mysql.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE)
+            logging.info(f"Connected to database!")
         except Exception as e:
             logging.critical(f"Unable to connect to database: {e}")
             exit(1)
-        self.start_index = 0
-        self.page_size = 100
+        self.start_index = start_index
+        self.page_size = page_size
         self.appId = appId
     
     def add_item(self, data: Item) -> bool:
-        return False
+        logging.info(f"Adding the item to database! {data}")
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(f"""
+                        INSERT INTO Item (itemNameId, itemHashName, itemName, itemIcon, gameId)
+                        VALUES ({data.itemNameId}, "{data.itemHashName}", "{data.itemName}", "{data.itemIcon}", {data.gameId})
+                        """)
+            cursor.close()
+            return True
+        except Exception as e:
+            logging.exception(f"An error occured when inserting into database! -> {data}")
+            return False
+    
+    def in_database(self, itemHashName : str) -> bool:
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(f"SELECT COUNT(*) as num FROM Item WHERE itemHashName = \"{itemHashName}\"")
+            toReturn = cursor.fetchone()['num']
+            logging.debug(toReturn)
+            cursor.close()
+            return toReturn
+        except Exception as e:
+            logging.exception("An exception occured when checking for item in database!")
     
     def populate_items(self):
         while True:
@@ -47,27 +73,72 @@ class PopulateItems:
                     "norender": 1,
                     "start": self.start_index
                 })
+                logging.debug(f"Requested {res.request}")
                 if res.status_code != 200:
-                    raise Exception(f"Status code was {res.status_code}")
+                    raise Exception(f"Status code for {ITEMS_URL} was {res.status_code}")
                 
                 res_json = res.json()
-                print(res_json)
+                logging.debug(f"Response JSON: {res_json}")
                 if not res_json['success']:
                     raise Exception(f"Success field was false")
                 
-                results = res_json.results
-                print(results)
+                results = res_json["results"]
+                logging.info(f"Items {self.start_index} to {self.start_index + self.page_size} received")
+                
+                for item in results:
+                    try:
+                        hashName = item["hash_name"]
+
+                        if self.in_database(hashName):
+                            logging.info(f"{hashName} already in database! Skipping!")
+                            continue
+
+                        page_url = f"{ITEM_PAGE_URL}/{self.appId}/{hashName}"
+                        logging.debug(f"Requesting {page_url}")
+                        r = requests.get(page_url)
+
+                        if r.status_code != 200:
+                            raise Exception(f"Status code for {page_url} was {r.status_code}")
+                        
+                        html = r.content
+                        soup = BeautifulSoup(html, 'html.parser')
+                        logging.debug(f"Response content: {soup.prettify()}")
+                        script = soup.find_all("script")[-1].string
+                        logging.debug(f"Script content: {script}")
+
+                        idx = script.find("Market_LoadOrderSpread")
+                        script = script[idx+25:]
+                        idx = script.find(")")
+                        script = script[:idx]
+                        script = script.strip()
+                        itemNameId = int(script)
+                        logging.debug(f"Found ItemNameId: {itemNameId}")
+
+                        marketItem = Item(itemNameId, hashName, item['name'], item['asset_description']['icon_url'], item['asset_description']['appid'])
+                        if not self.add_item(marketItem):
+                            logging.warning("Failed to insert item into database!")
+                            """Do nothing else for now, just skip"""
+
+                        sleep(10)
+                        
+                    except Exception as e:
+                        logging.exception(f"An error occured when doing an item market request")
+                        sleep(60)
+
+
+
             except Exception as e:
-                logging.warning(f"An error occured when doing a request: {e}")
+                logging.exception(f"An error occured when doing an item batch request")
                 sleep(60)
                 continue
             
-            if True:
+            if len(results) == 0: # Results is empty
                 break
 
             self.start_index += self.page_size
         
         logging.info("Script finished!")
+
 
 
 
